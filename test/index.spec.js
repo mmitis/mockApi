97cfd3fb93b6bb49e958ef3195efcf5d3a30d977
Marek <likon1@likon1.pl>
2016-01-18 18:52:57 +0100

    fix error terst

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,7 @@
 describe('Checks if all configs file exists', function(){
 
     it('Checks config.js', function(){
-        expect(true).toBe('true');
+        expect(true).toBe(true);
     });
 
 
